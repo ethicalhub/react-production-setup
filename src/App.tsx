58a872a215b332e2caa0689cb11ec04b1f038f41
ethicalhub <yasiransari.elith@gmail.com>
2025-07-02 14:59:15 +0530

    feat: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-    return <div>React Production Setup</div>
+    return <div className="text-red-400 flex items-center min-h-screen justify-center text-5xl font-bold italic underline underline-offset-2">React Production Setup</div>
 }
 
 export default App
