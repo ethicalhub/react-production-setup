8c95a3b01b3786edf55e45788b5bb4565fae329e
ethicalhub <yasiransari.elith@gmail.com>
2025-07-02 15:40:59 +0530

    feat: lint-staged setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ const config = {
     '*.css': ['yarn run lint:stylelint', 'yarn run format:check'],
     '*.json': ['yarn run format:check'],
     '*.md': ['yarn run format:check'],
-    '*.{html}': ['yarn run format:check']
+    '*.html': ['yarn run format:check']
 }
 
-export default config;
+export default config
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 const App = () => {
-  console.log('React Production Setup');
-    return <div className="text-red-400 flex items-center min-h-screen justify-center text-5xl font-bold italic underline underline-offset-2">React Production Setup</div>
+    return (
+        <div className="text-red-400 flex items-center min-h-screen justify-center text-5xl font-bold italic underline underline-offset-2">
+            React Production Setup
+        </div>
+    )
 }
 
 export default App
