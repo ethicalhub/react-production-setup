02634edf7c39a6f9965af1e38a9edbdca73672a6
ethicalhub <yasiransari.elith@gmail.com>
2025-07-07 00:13:04 +0530

    feat(setup): github actions yarn pkg

--- src/features/feature2/Feature2Page.tsx
+++ src/features/feature2/Feature2Page.tsx
@@ -1,5 +1,12 @@
 const Feature2Page = () => {
-    return <div>Feature2Page</div>
+    return (
+        <div>
+            Feature2Page Part 2<p>This is the second part of Feature 2.</p>
+            <p>
+                It is designed to demonstrate the continuation of a feature across multiple pages.
+            </p>
+        </div>
+    )
 }
 
 export default Feature2Page
